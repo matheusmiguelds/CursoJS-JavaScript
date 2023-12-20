06cb32e93d81beb5ff7e854136a1813eb1d2d629
Matheus <matheusmiguelds@gmail.com>
2023-12-20 10:29:19 -0300

    Iniciando minha calculadora em JS

---
+++ CalculadoraJS/calculadora/assets/js/main.js
@@ -0,0 +1,27 @@
+function criaCalculadora() {
+    return {
+        display: document.querySelector(".display"),
+
+        inicia() {
+            this.cliqueBotoes();
+        },
+
+        cliqueBotoes() {
+            document.addEventListener('click', function(e){
+                const el = e.target;
+
+                if(el.classList.contains('btn-num')){
+                    this.btnParaDisplay();
+                }
+            }.bind(this));
+        },
+
+        btnParaDisplay() {
+         
+        }
+
+    };
+}
+
+const calculadora = criaCalculadora();
+calculadora.inicia();

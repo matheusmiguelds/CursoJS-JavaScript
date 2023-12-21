7bef16839ad819909ef08a596d140977e3dffb5c
Matheus <matheusmiguelds@gmail.com>
2023-12-21 18:57:30 -0300

    Finalizando a calculadora

--- CalculadoraJS/calculadora/assets/js/main.js
+++ CalculadoraJS/calculadora/assets/js/main.js
@@ -1,23 +1,69 @@
 function criaCalculadora() {
     return {
         display: document.querySelector(".display"),
+        btnClear: document.querySelector(".btn-clear"),
 
         inicia() {
             this.cliqueBotoes();
+            this.enter();
+        },
+
+        enter(){
+            this.display.addEventListener('keyup', e => {
+                if(e.keyCode === 13) {
+                    this.result();
+                }
+            });
+        },
+
+        clearDisplay() {
+            this.display.value = '';
+        },
+
+        deleteOne() {
+            this.display.value = this.display.value.slice(0, -1);
+        },
+
+        result() {
+            let conta = this.display.value;
+
+            try {
+                conta = eval(conta);
+                if(!conta) {
+                    alert("Conta inválida");
+                }
+
+                this.display.value = conta;
+
+            } catch (e) {
+                alert("Conta inválida");
+            }
         },
 
         cliqueBotoes() {
-            document.addEventListener('click', function(e){
+            document.addEventListener('click', (e) => {
                 const el = e.target;
 
                 if(el.classList.contains('btn-num')){
-                    this.btnParaDisplay();
+                    this.btnParaDisplay(el.innerText);
                 }
-            }.bind(this));
+
+                if(el.classList.contains("btn-clear")){
+                    this.clearDisplay();
+                }
+
+                if(el.classList.contains("btn-del")){
+                    this.deleteOne();
+                }
+
+                if(el.classList.contains("btn-eq")){
+                    this.result();
+                }
+            });
         },
 
-        btnParaDisplay() {
-         
+        btnParaDisplay(valor) {
+            this.display.value += valor;
         }
 
     };

212caa46c56845fea9b98edb47a973da8a8dbed6
Matheus <MatheusMiguelds@gmail.com>
2024-01-29 15:48:21 -0300

    Atualizando a Seção 03

---
+++ Secao-03/aula21 - Operadores/index.js
@@ -0,0 +1,13 @@
+/*
+> maior que
+>= maior ou igual a
+< menor que
+<= menor ou igual a
+== igual (valor)
+=== igual estrito (valor e tipo)
+!= diferente (valor)
+!== diferente estrito (valor e tipo)
+*/
+
+const comp = 10 === '10';
+console.log(comp);
--- Secao3/aula25 - for/index.js
+++
@@ -1,5 +0,0 @@
-const frutas = ["Maçã", "Pêra", "Uva", "Melancia", "Banana", "Abacaxi"];
-
-for (let i = 0; i < frutas.length; i++) {
-    console.log(`Indice ${i}`, frutas[i]);
-}

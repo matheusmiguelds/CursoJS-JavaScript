a4c829b5178d6ebb4622fa411254732edb8d09e6
Matheus <MatheusMiguelds@gmail.com>
2024-02-19 08:32:36 -0300

    Adicionando conteúdo sobre functions

--- Secao-04/aula35-retorno/index.js
+++ Secao-04/aula35-retorno/index.js
@@ -1,23 +1,36 @@
-function soma(a, b) {
-    return a + b;
-}
+// function soma(a, b) {
+//     return a + b;
+// }
 
-function soma2(a, b){
-    console.log(a + b);
-}
-soma2(5, 2);
+// function soma2(a, b){
+//     console.log(a + b);
+// }
+// soma2(5, 2);
 
-function criaPessoa(nome, sobrenome){
-    return { nome, sobrenome };
-}
-const p1 = criaPessoa('Matheus', 'Miguel');
-console.log(p1);
+// function criaPessoa(nome, sobrenome){
+//     return { nome, sobrenome };
+// }
+// const p1 = criaPessoa('Matheus', 'Miguel');
+// console.log(p1);
 
-function falaFrase(comeco) {
-    function falaResto(resto) {
-        return comeco + ' ' + resto;
-    }
-    return falaResto;
+// function falaFrase(comeco) {
+//     function falaResto(resto) {
+//         return comeco + ' ' + resto;
+//     }
+//     return falaResto;
+// }
+// const olaMundo = falaFrase('Olá');
+// console.log(olaMundo('mundo'));
+
+function criaMultiplicador(multiplicador) {
+    return function(n) {
+        return n * multiplicador;
+    };
 }
-const olaMundo = falaFrase('Olá');
-console.log(olaMundo('mundo'));
+const duplica = criaMultiplicador(2);
+const triplica = criaMultiplicador(3);
+const quadriplica = criaMultiplicador(4);
+
+console.log(duplica(2));
+console.log(triplica(2));
+console.log(quadriplica(2));

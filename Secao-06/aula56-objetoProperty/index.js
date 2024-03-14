14ed3110798c28bd38e309bd5381589e006713b3
Matheus <MatheusMiguelds@gmail.com>
2024-03-13 22:35:32 -0300

    defineProperty

---
+++ Secao-06/aula56-objetoProperty/index.js
@@ -0,0 +1,24 @@
+// defineProperty - defineProperties
+function Produto(nome, preco, estoque) {
+    this.nome = nome;
+    this.preco = preco;
+
+    Object.defineProperty(this, 'estoque', {
+        enumerable: true, // Mostra a chave
+        value: estoque, // Mostra o valor
+        writable: false, // Posso alterar?
+        configurable: true // Posso apagar/ reconfigurar?
+    });
+
+    // Object.defineProperty(this, 'estoque', {
+    //     enumerable: true, // Mostra a chave
+    //     value: estoque, // Mostra o valor
+    //     writable: true, // Posso alterar?
+    //     configurable: false // Posso apagar/ reconfigurar?
+    // });
+}
+
+const p1 = new Produto('Camiseta', 20, 3);
+// console.log(p1.nome); // Variavel publica
+p1.estoque = 50000;
+console.log(p1);

802a9920f15cd61fc42e4da6cf1daff31c35a9b8
Matheus <MatheusMiguelds@gmail.com>
2024-03-13 22:22:48 -0300

    Terminando a revisão sobre Objetos

--- Secao-06/aula55-revisando/index.js
+++ Secao-06/aula55-revisando/index.js
@@ -38,7 +38,10 @@
 function Pessoa(nome, sobrenome) {
     this.nome = nome;
     this.sobrenome = sobrenome;
+    Object.freeze(this);
 }
 
 const p1 = new Pessoa('Matheus', 'Miguel');
+// Object.freeze(p1); // Não permite mudanças no objeto
+p1.nome = 'Outra coisa'
 console.log(p1);

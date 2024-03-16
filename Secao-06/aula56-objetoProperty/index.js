a47085ab8fc256beed2b35f84b8c2d643d590d5d
Matheus <MatheusMiguelds@gmail.com>
2024-03-16 15:32:52 -0300

    Finalizando defineProperty

--- Secao-06/aula56-objetoProperty/index.js
+++ Secao-06/aula56-objetoProperty/index.js
@@ -16,9 +16,25 @@ function Produto(nome, preco, estoque) {
     //     writable: true, // Posso alterar?
     //     configurable: false // Posso apagar/ reconfigurar?
     // });
+
+    Object.defineProperties(this, {
+        nome: {
+            enumerable: true, // Mostra a chave
+            value: nome, // Mostra o valor
+            writable: true, // Posso alterar?
+            configurable: true // Posso apagar/ reconfigurar?
+        },
+        preco: {
+            enumerable: true, // Mostra a chave
+            value: preco, // Mostra o valor
+            writable: true, // Posso alterar?
+            configurable: true // Posso apagar/ reconfigurar?
+        },
+    });
 }
 
 const p1 = new Produto('Camiseta', 20, 3);
 // console.log(p1.nome); // Variavel publica
-p1.estoque = 50000;
-console.log(p1);
+// p1.estoque = 50000;
+// console.log(p1);
+console.log(Object.keys(p1)); // Enumerable está como true, assim mostra
